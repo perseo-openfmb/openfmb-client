de172f1872292d576a47802b5dcd5d5771230f6e
kevindmzapata <kevin.martinez1@udea.edu.co>
2026-02-07 09:37:21 -0500

    Bump version to 0.3.0 and remove unnecessary dependencies from setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="openfmb-client",            # Nombre para pip
-    version="0.2.0",
+    version="0.3.0",
     description="Cliente Python para interactuar con la API de OpenFMB en microgrids",
     author="Kevin Martinez",
     author_email="kevin9907martinez@gmail.com",
     packages=find_packages(),         # Busca automáticamente la carpeta openfmb_client
     install_requires=[
         "requests>=2.25.0",         # Instalará requests automáticamente
-        "logging",                  # Para el manejo de logs
-        "typing",                   # Para anotaciones de tipos
-        "datetime",                 # Para manejo de fechas y horas
-        "uuid",                     # Para manejo de UUIDs
         "pandas"                    # Sugerencia: Si vas a añadir soporte para DataFrames
     ],
     python_requires=">=3.8",
